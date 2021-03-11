1527877d67fd7eda5eace3208aed900aaaabb5ce
navetacandra24 <naveta.cand@gmail.com>
2021-03-11 12:36:07 +0700

    add Apakah command

--- app/commands/apakah.js
+++ app/commands/apakah.js
@@ -7,7 +7,15 @@ const handler = {
     async exec({m, args, client}) {
         const pertanyaan = m.body.replace(m.body.charAt(0), '');
         const jawaban = pickRandom(['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin']);
-        m.reply(`*Pertanyaan:* ${pertanyaan}\n*Jawaban:* ${jawaban}`)
+        if (m.mentionedIds) {
+            const contact = []
+            for (let i = 0; i < m.mentionedIds.length; i++) {
+                contact.push(await client.getContactById(m.mentionedIds[i]))
+            }
+            m.reply(`*Pertanyaan:* ${pertanyaan}\n*Jawaban:* ${jawaban}`, m.from, {mentions: contact})
+        } else {
+            m.reply(`*Pertanyaan:* ${pertanyaan}\n*Jawaban:* ${jawaban}`)
+        }
     }
 }
 
