5cad104e8cfb936df0d2bf0dc0b5518743d106e9
navetacandra24 <naveta.cand@gmail.com>
2021-03-15 13:48:26 +0700

    ?

--- app/commands/attp.js
+++ app/commands/attp.js
@@ -5,18 +5,22 @@ const attp = require(`${__dirname}/../helper/attp`)
 const handler = {
     name: 'attp',
     async exec({m, args, MessageMedia, client}) {
-        await m.reply('Memproses..')
+        m.reply('Memproses..')
         const text = args.join(' ');
         await attp(text)
 
         webp.grant_permission();
 
         const result = webp.gwebp(`${__dirname}/../src/attp.gif`, `${__dirname}/../src/attp.webp`, "-q 80", logging = "-v");
-        result.then(async e => {
-            let media = MessageMedia.fromFilePath(`${__dirname}/../src/attp.webp`)
-            await m.reply(media, m.from, {sendMediaAsSticker: true})
-            // client.sendMessage(m.from, media, {sendMediaAsSticker: true})
-        });
+        result
+            .then(async e => {
+                let media = MessageMedia.fromFilePath(`${__dirname}/../src/attp.webp`)
+                await m.reply(media, m.from, { sendMediaAsSticker: true })
+                // client.sendMessage(m.from, media, {sendMediaAsSticker: true})
+            })
+            .catch(err => {
+                m.reply(err)
+            })
     }
 }
 
--- app/commands/ttp.js
+++ app/commands/ttp.js
@@ -5,17 +5,16 @@ const fs = require('fs')
 const handler = {
     name: 'ttp',
     async exec({m, args, MessageMedia, client}) {
-        await m.reply('Memproses..')
+        m.reply('Memproses..')
         const text = args.join(' ');
         await ttp(text)
 
         webp.grant_permission();
 
         const result = webp.cwebp(`${__dirname}/../src/ttp.jpg`, `${__dirname}/../src/ttp.webp`, '-q 80', logging = '-v')
-            result.then(async () => {
-                // console.log('Compile is done');
+            result.then(() => {
                 let media = MessageMedia.fromFilePath(`${__dirname}/../src/ttp.webp`)
-                await m.reply(media, m.from, { sendMediaAsSticker: true })
+                m.reply(media, m.from, { sendMediaAsSticker: true })
             })
             .catch(err => {
                 console.log(err);
