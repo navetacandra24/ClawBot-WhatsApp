8ea0f24d06c62d4b1bc70f879acec1c3f081b115
navetacandra24 <naveta.cand@gmail.com>
2021-03-18 18:40:09 +0700

    ?

--- app/commands/gtranslate.js
+++ app/commands/gtranslate.js
@@ -9,20 +9,24 @@ const handler = {
     },
     async exec({ m, args }) {
 
-        let from = args[0]
-        let to = args[1]
-        args.splice(0, 2);
-        let _text = args.join(' ');
-        // console.log(text_b);
+        if (args.length < 3) {
+            m.reply(`Masukkan parameter dengan benar, Contoh: \n#gtranslate indonesia inggris Halo dunia`)
+        } else {
+            let from = args[0]
+            let to = args[1]
+            args.splice(0, 2);
+            let _text = args.join(' ');
+            // console.log(text_b);
 
-        try {
-            let translated = await gtranslate(from, to, _text);
-            let result = `*${lang[from]} :* ${_text}\n*${lang[to]} :* ${translated.result.translated}`;
-            // console.log(result);
-            m.reply(result)
-        } catch (err) {
-            // console.log(err);
-            m.reply(err)
+            try {
+                let translated = await gtranslate(from, to, _text);
+                let result = `*${lang[from]} :* ${_text}\n*${lang[to]} :* ${translated.result.translated}`;
+                // console.log(result);
+                m.reply(result)
+            } catch (err) {
+                // console.log(err);
+                m.reply(err)
+            }
         }
 
 
--- app/commands/utranslate.js
+++ app/commands/utranslate.js
@@ -14,26 +14,26 @@ const lang = {
 
 
 const handler = {
-    command: /utranslate/,
-    helper: function () {
-        return lib(this.command).map(v => '#' + v + ' <bahasa asal> <bahasa tujuan> <teks> (Support Inggris & Indonesia only)')
-    },
     async exec({ m, args }) {
 
-        let from = args[0]
-        let to = args[1]
-        args.splice(0, 2);
-        let _text = args.join(' ');
-        // console.log(text_b);
+        if (args.length < 3) {
+            m.reply(`Masukkan parameter dengan benar, Contoh: \n#utranslate indonesia inggris Halo dunia`)
+        } else {
+            let from = args[0]
+            let to = args[1]
+            args.splice(0, 2);
+            let _text = args.join(' ');
+            // console.log(text_b);
 
-        try {
-            let translated = await utranslate(from, to, _text);
-            let result = `*${lang[from]} :* ${_text}\n*${lang[to]} :* ${translated.result.translated}`;
-            // console.log(result);
-            m.reply(result)
-        } catch (err) {
-            // console.log(err);
-            m.reply(err)
+            try {
+                let translated = await utranslate(from, to, _text);
+                let result = `*${lang[from]} :* ${_text}\n*${lang[to]} :* ${translated.result.translated}`;
+                // console.log(result);
+                m.reply(result)
+            } catch (err) {
+                // console.log(err);
+                m.reply(err)
+            }
         }
 
 
