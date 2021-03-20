6215361812684efb950103f7ce049b05812415f6
navetacandra24 <naveta.cand@gmail.com>
2021-03-20 13:01:22 +0700

    ?

--- app/commands/glitchtext.js
+++ app/commands/glitchtext.js
@@ -8,17 +8,17 @@ const handler = {
                 await m.reply('Memproses..\n*Mohon tunggu sekitar 1 menit.*')
                 let link = `http://fierce-brushlands-90323.herokuapp.com/glitch-text?text1=${encodeURIComponent(_ft[0])}&text2=${encodeURIComponent(_ft[1])}`;
             
-                try {
-                    let _fetch = await fetch(link, {
-                        mode: 'no-cors',
-                        timeout: 100 * 60 * 60
-                    });
-                    let _res = await _fetch.json();
-                    let _base64 = await _res.results.data.base64;
+                let _fetch = await fetch(link, {
+                    mode: 'no-cors',
+                    timeout: 36000*24
+                });
+                let _res = await _fetch.json();
+                let _base64 = await _res.results.data.base64;
+                if (_base64 === '' || !_base64.startsWith('/9j/4AAQSkZJRgABAQAAAQABAAD//')) {
+                    m.reply('*Maaf gambar tidak dapat dikirim karena terjadi kesalahan system*')
+                } else {
                     let media = new MessageMedia('image/jpeg', _base64, '');
                     m.reply(media)
-                } catch (err) {
-                    m.reply(err)
                 }
             } else {
                 m.reply('Masukkan format dengan benar\n*Contoh :* #glitchtext Clawbot | GG Gaming')
