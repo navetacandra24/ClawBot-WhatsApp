a725ff0c307c48df8fc397ff78f252cd62ea82ce
navetacandra24 <naveta.cand@gmail.com>
2021-03-22 16:02:37 +0700

    ?

--- app/commands/kick.js
+++ app/commands/kick.js
@@ -2,7 +2,7 @@ const handler = {
     async exec({ args, client, m }) {
         
         if (m.from.includes('6285311174928')) {
-            let chat = await m.getChats(m.from);
+            let chat = await client.getChatById(m.from);
             console.log(chat);
         } else {
             m.reply('Kamu siapa? Perintah ini khusus *OWNER*')
