a0c56d6f41554387b2209036ceefa42f420ba570
navetacandra24 <naveta.cand@gmail.com>
2021-03-26 17:04:37 +0700

    ??

--- app/commands/help.js
+++ app/commands/help.js
@@ -2,7 +2,7 @@ const commandsDB = require(`${__dirname}/../commands-database`);
 const readmore = String.fromCharCode(8206).repeat(4001);
 
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) + 7
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
     return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(' : ')
