cad721e41f18dfdfbf12daa1d8d1216d44b15f80
navetacandra24 <naveta.cand@gmail.com>
2021-04-01 16:05:27 +0700

    ?

--- app/commands/kbbi.js
+++ app/commands/kbbi.js
@@ -7,14 +7,14 @@ const handler = {
         const html = cheerio.load(await rp(url))('div[class="container body-content"]').html();
         const _ = cheerio.load(html);
         let existRes = _('ol').html() ? true : false;
-        let existRes1 = _('ul').html() ? true : false;
+        let existRes1 = _('ul[class="adjusted-par"]').html() ? true : false;
         if (existRes || existRes1) {
             let fullTitle = _('h2').text()
             let title = fullTitle.includes(' » ')
                 ? fullTitle.split(' » ')[1]
                 : fullTitle
             let arti = [];
-            let root = existRes ? 'ol' : 'ul'
+            let root = existRes ? 'ol' : 'ul[class="adjusted-par"]'
             _(root).find('font[color="red"]').remove().html();
 
             _(`${root} > li`).each((i, el) => {
